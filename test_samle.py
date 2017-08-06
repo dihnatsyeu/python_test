78594ebe409473cd98ade87a126af5d0addeaf9e
Dzmitry Ihnatsyeu <d.ihnatsyeu@gmail.com>
2017-08-06 13:58:26 +0300

    modifications

--- test_samle.py
+++ test_samle.py
@@ -4,3 +4,5 @@ class TestMyFunc(object):
     def test_my_site(self):
         with pytest.allure.step("MyStep"):
             print("mybad")
+        with pytest.allure.step("AnotherStep"):
+            print "Ups i did it again"
